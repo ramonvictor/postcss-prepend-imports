097004d468f679d299b00f171145e99c5762cd9a
Arsen Karapetyan <a.karapetyan@corp.mail.ru>
2019-06-24 19:50:30 +0300

    feat: new options for add import after all imports

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ module.exports = postcss.plugin('postcss-prepend-imports', function (opts) {
     opts = opts || {};
     opts.path = opts.path || '';
     opts.files = opts.files || [];
+    opts.afterAllImports = opts.afterAllImports || false;
 
     return function (root) {
         opts.files.reverse().forEach(file => {
             file = path.resolve(opts.path, file);
-            root.prepend({ name: 'import', params: '"' + file + '"' });
+            if (opts.afterAllImports) {
+                let lastImport = null;
+                root.nodes.forEach(node => {
+                    if (node.type === 'atrule' && node.name === 'import') {
+                        lastImport = node;
+                    }
+                });
+                lastImport.after({ name: 'import', params: '"' + file + '"' });
+            } else {
+                root.prepend({ name: 'import', params: '"' + file + '"' });
+            }
         });
     };
 });
